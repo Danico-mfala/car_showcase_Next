7391de9f743eec4b856fa50841833d835fd5ab7e
Danico-Mfala <mfaladan@hotmail.com>
2023-07-17 09:02:54 +0300

    tailwind seting up

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero'>
+          <div className="flex pt-36 padding-x">
+              <h1>Hello World</h1>
+          </div>
     </div>
   )
 }
